0d8165ba57fba9392a33518c0cb9eafc5d34b645
Pavel Shramov <psha@nguni.psha.org.ru>
2021-09-14 08:41:32 +0300

    scheme: Replace header with autogenerated one

--- src/netlink.h
+++ src/netlink.h
@@ -5,7 +5,15 @@
 #pragma pack(push, 1)
 namespace netlink_scheme {
 
-enum class Action : int8_t { New = 0, Delete = 1 };
+static constexpr std::string_view scheme_string =
+    R"(yamls+gz://eJzlU01vgkAQvfdX7G0vmIhaar2tSIIpFbOitacGZW02IhBZ2hrjf+8sH1q7Gtt67Ik3zJuP9wZqKPJXrIMwvkGIRdkq7QBACJO54HGEO2grNgkQeCTaWs6AV7jHQiYYZHUN4QF7B1Tf7YpK6kHBuUIyeDbJyIN0Cyq7DjEfbNexIDZkTF3SK/NNiB3XJA7gBuBHMpXzcjh2vH5Ju5ULEIn0OsAhde1+ty9jmIo9m7pPgO8BjwdVjZ5H1CKmTbr58Lv8zWhomVIJBFNqjVxnInO6nivzQ+6nrLSnhralbzx5a2ENFVpxBmqbDbxTScaBNNsIphsqKRJsvfDnTGFqKE7kNVLpoCjcxalY8+gVw2616oYOj5byjjyQbgBYcBYGysp+cdr9lPLU6j4B+ziwTiuTj9/ue9whS47ta+MvimicCdaqNDWu1iT8WcgunitfeU8qvufvnJgvrtMdpOJl5adLVb3Cu7hwup7/qBfwTvQ6dtuo3G7+e7fP/Kh/cnvf6xMdeI6x)";
+
+enum class Action : int8_t
+{
+	New = 0,
+	Delete = 1,
+};
+
 enum class RType : int8_t
 {
 	UNSPEC = 0,
@@ -23,43 +31,39 @@ enum class RType : int8_t
 	MAX = 12,
 };
 
-typedef uint32_t ipv4;
-typedef tll::scheme::Bytes<16> ipv6;
-typedef tll::scheme::ByteString<16> interface;
-
 struct Link
 {
-	static const int msgid = 10;
+	static constexpr int msgid = 10;
 	Action action;
 	int32_t index;
-	interface name;
+	tll::scheme::ByteString<16> name;
 	uint8_t up;
 };
 
 struct Route4
 {
-	static const int msgid = 20;
+	static constexpr int msgid = 20;
 	Action action;
 	uint32_t table;
 	RType type;
-	interface oif;
+	tll::scheme::ByteString<16> oif;
 	uint8_t dst_mask;
-	ipv4 dst;
+	uint32_t dst;
 	uint8_t src_mask;
-	ipv4 src;
+	uint32_t src;
 };
 
 struct Route6
 {
-	static const int msgid = 30;
+	static constexpr int msgid = 30;
 	Action action;
 	uint32_t table;
 	RType type;
-	interface oif;
+	tll::scheme::ByteString<16> oif;
 	uint8_t dst_mask;
-	ipv6 dst;
+	tll::scheme::Bytes<16> dst;
 	uint8_t src_mask;
-	ipv6 src;
+	tll::scheme::Bytes<16> src;
 };
 
 } // namespace netlink_scheme
